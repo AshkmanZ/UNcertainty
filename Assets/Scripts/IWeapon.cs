3e2f09f74a3e79ab07fd9d8e77786959c8351fde
Ashkman <62811176+Ashkman@users.noreply.github.com>
2020-08-24 18:52:22 +0430

    Revert "uploaded"
    This reverts commit 6dbc143b95bcb0fcd01b3bda37eaa5e664974c5d.

--- Assets/Scripts/IWeapon.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IWeapon
-{
-    void Attack(Vector2 force,float attackSpeed = 1);
-    void SetColor(Material color, string colorString);
-}
--- Assets/Scripts/Weapon.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Weapon : MonoBehaviour
-{
-    public Sprite sprite;
-}
